b6ea0bb0ed75fd9580342ffa743f99295167f022
ekank24 <ekankkun24@students.oamk.fi>
2025-12-08 00:05:31 +0200

    node

---
+++ callback_example.js
@@ -0,0 +1,7 @@
+setTimeout(doSomething, 2000);
+
+function doSomething() {
+    console.log("Demonstrating the callbacks");
+}
+
+console.log("The application is started");
---
+++ nuoli_example.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+    console.log("Demonstrating the callbacks with an arrow function");
+}, 2000);
+
+console.log("The application is started");
